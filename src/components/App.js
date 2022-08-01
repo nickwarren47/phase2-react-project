e9dd5a2be9857e1e4a54c0aa397807155c83e195
Nick Warren <nickwarren47@gmail.com>
2022-08-01 17:59:56 -0600

    Added header and css for header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ItemPage from "./ItemPage";
+import Header from "./Header";
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
+      <br />
       <ItemPage />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+            <h1>
+                Coder's Keys 
+                <span className="logo" role="img">
+                    💻
+                </span>
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
